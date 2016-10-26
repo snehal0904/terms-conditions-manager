5e7205e6e37e73b123ec2f86fd0f4fd0ad969f0f
Deepali <deepali_m@tekditechnologies.com>
2016-10-26 16:40:30 +0530

    redirect URL to home page if user logout before accepts the T&C

--- com_tc/site/views/content/view.html.php
+++ com_tc/site/views/content/view.html.php
@@ -30,9 +30,17 @@ class TcViewContent extends JViewLegacy
 	 */
 	public function display($tpl = null)
 	{
+		$app        = JFactory::getApplication();
 		$input = JFactory::getApplication()->input;
 		$this->content_id = $input->get('content_id', '', 'INT');
 		$this->user_id    = JFactory::getUser()->id;
+
+		if (!$this->user_id)
+		{
+			$app->redirect(JRoute::_(JURI::base()));
+
+			return false;
+		}
 
 		require_once JPATH_ADMINISTRATOR . '/components/com_tc/models/content.php';
 
