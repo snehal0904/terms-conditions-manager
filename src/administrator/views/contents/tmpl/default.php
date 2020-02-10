3cd0991f0312b35a8b05c9347281405df147390a
Snehal Patil <snehal_patil@techjoomla.com>
2020-02-10 10:56:44 +0530

    Overall minor improvements and bug fixes

--- src/administrator/views/contents/tmpl/default.php
+++ src/administrator/views/contents/tmpl/default.php
@@ -11,35 +11,15 @@
 defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/');
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Layout\LayoutHelper;
-JHtml::_('bootstrap.tooltip');
-JHtml::_('behavior.multiselect');
-JHtml::_('formbehavior.chosen', 'select');
+HTMLHelper::_('behavior.tooltip');
+HTMLHelper::_('behavior.multiselect');
+HTMLHelper::_('formbehavior.chosen', 'select');
 
 $listOrder = $this->state->get('list.ordering');
 $listDirn  = $this->state->get('list.direction');
 ?>
-<script type="text/javascript">
-	Joomla.orderTable = function () {
-		table = document.getElementById("sortTable");
-		direction = document.getElementById("directionTable");
-		order = table.options[table.selectedIndex].value;
-		if (order != '<?php echo $listOrder; ?>') {
-			dirn = 'asc';
-		} else {
-			dirn = direction.options[direction.selectedIndex].value;
-		}
-		Joomla.tableOrdering(order, dirn, '');
-	};
-
-	jQuery(document).ready(function () {
-		jQuery('#clear-search-button').on('click', function () {
-			jQuery('#filter_search').val('');
-			jQuery('#adminForm').submit();
-		});
-	});
-</script>
-
 <form action="<?php echo JRoute::_('index.php?option=com_tc&view=contents'); ?>" method="post" name="adminForm" id="adminForm">
 	<?php
 	if (!empty($this->sidebar)): ?>
@@ -64,31 +44,26 @@ $listDirn  = $this->state->get('list.direction');
 			<table class="table table-striped" id="contentList">
 				<thead>
 				<tr>
-					<th width="1%" class="hidden-phone">
-						<input type="checkbox" name="checkall-toggle" value=""
-							   title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)"/>
+					<th class="hidden-phone">
+						<?php echo HTMLHelper::_('grid.checkall'); ?>
 					</th>
-					<?php
-					if (isset($this->items[0]->state)): ?>
-					<th width="1%" class="nowrap center">
-							<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.`state`', $listDirn, $listOrder); ?>
-					</th>
-					<?php
-					endif; ?>
-					<th class='left'>
-						<?php echo JHtml::_('grid.sort',  'COM_TC_CONTENTS_TITLE', 'a.`title`', $listDirn, $listOrder); ?>
+					<th>
+						<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder);?>
 					</th>
 					<th class='left'>
-						<?php echo JHtml::_('grid.sort',  'COM_TC_CONTENTS_VERSION', 'a.`version`', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort',  'COM_TC_CONTENTS_TITLE', 'a.title', $listDirn, $listOrder); ?>
 					</th>
 					<th class='left'>
-						<?php echo JHtml::_('grid.sort',  'COM_TC_CONTENTS_CLIENT', 'a.`client`', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort',  'COM_TC_CONTENTS_VERSION', 'a.version', $listDirn, $listOrder); ?>
 					</th>
 					<th class='left'>
-						<?php echo JHtml::_('grid.sort',  'COM_TC_CONTENTS_START_DATE', 'a.`start_date`', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort',  'COM_TC_CONTENTS_CLIENT', 'a.client', $listDirn, $listOrder); ?>
 					</th>
 					<th class='left'>
-						<?php echo JHtml::_('grid.sort',  'COM_TC_CONTENTS_ID', 'a.`tc_id`', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort',  'COM_TC_CONTENTS_START_DATE', 'a.start_date', $listDirn, $listOrder); ?>
+					</th>
+					<th class='left'>
+						<?php echo HTMLHelper::_('searchtools.sort',  'COM_TC_CONTENTS_ID', 'a.tc_id', $listDirn, $listOrder); ?>
 					</th>
 				</tr>
 				</thead>
@@ -109,18 +84,18 @@ $listDirn  = $this->state->get('list.direction');
 					?>
 					<tr class="row<?php echo $i % 2; ?>">
 						<td class="hidden-phone">
-							<?php echo JHtml::_('grid.id', $i, $item->tc_id); ?>
+								<?php echo HTMLHelper::_('grid.id', $i, $item->tc_id); ?>
 						</td>
 							<?php
 							if (isset($this->items[0]->state)): ?>
-							<td class="center">
-								<?php echo JHtml::_('jgrid.published', $item->state, $i, 'contents.', $canChange, 'cb'); ?>
+								<td>
+									<?php echo HTMLHelper::_('jgrid.published', $item->state, $i, 'contents.', $canChange, 'cb'); ?>
 					</td>
 							<?php
 							endif; ?>
 			<td>
 				<?php if (isset($item->checked_out) && $item->checked_out && ($canEdit || $canChange)) : ?>
-					<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'contents.', $canCheckin); ?>
+									<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'contents.', $canCheckin); ?>
 				<?php endif; ?>
 				<?php if ($canEdit) : ?>
 					<a href="<?php echo JRoute::_('index.php?option=com_tc&task=content.edit&tc_id='.(int) $item->tc_id); ?>">
@@ -150,8 +125,6 @@ $listDirn  = $this->state->get('list.direction');
 		endif; ?>
 			<input type="hidden" name="task" value=""/>
 			<input type="hidden" name="boxchecked" value="0"/>
-			<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
-			<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
 			<?php echo JHtml::_('form.token'); ?>
 		</div>
 </form>
